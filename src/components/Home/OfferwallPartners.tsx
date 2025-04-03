c155ebb34aa3a02ce32d67f4d9aacb80b7bb3800
Yousief Sameh <yousief.sameh@outlook.com>
2025-04-03 23:32:29 +0200

    Add Hover In Offerwall Partners

--- src/components/Home/OfferwallPartners.tsx
+++ src/components/Home/OfferwallPartners.tsx
@@ -1,6 +1,7 @@
 import SpecialHeading from "@components/SpecialHeading";
 import { BsJoystick } from "react-icons/bs";
 import { FaStar } from "react-icons/fa";
+import { FaRegCirclePlay } from "react-icons/fa6";
 
 const OfferwallPartners = () => {
   return (
@@ -16,13 +17,15 @@ const OfferwallPartners = () => {
               key={index}
               className="card p-3 flex justify-center flex-col hover:scale-105 transform transition duration-300"
             >
-              <div className="relative w-full h-full bg-gradient-to-b from-[#1D232A] via-[#1D232A] to-[#1D232A] min-h-[13rem] min-w-[10rem] flex flex-col items-center justify-center rounded-lg shadow-lg hover:shadow-2xl hover:bg-gradient-to-b hover:from-[#3b7d67] transition-colors border border-[#3b7d67]">
+              <div className="relative w-full h-full bg-gradient-to-b from-[#1D232A] via-[#1D232A] to-[#1D232A] min-h-[13rem] min-w-[10rem] flex flex-col items-center justify-center rounded-lg shadow-lg hover:shadow-2xl hover:bg-gradient-to-b hover:from-[#3b7d67] transition-colors border border-[#3b7d67] group">
+                <div className="overlay absolute top-0 left-0 h-full w-full backdrop-blur-[5px] hidden items-center justify-center text-none text-5xl z-[3] rounded-lg cursor-pointer group-hover:flex" ><FaRegCirclePlay className="text-secondary-color" /></div>
+                <span className="badge badge-soft badge-success text-white absolute top-2 right-2">+20%</span>
                 <img
                   src="https://coinsmakers.com/asset/uploads/offerwalls/67bc4d8226c2d1740393858.png"
                   className="w-[100px] mb-6 hover:scale-110 transition-transform duration-300 drop-shadow-lg"
                   alt="Card Image"
                 />
-                <div className="text absolute bottom-0 flex flex-col justify-center items-center gap-1 w-full py-2 rounded-b-lg">
+                <div className="text absolute bottom-0 flex flex-col justify-center items-center gap-1 w-full py-2 rounded-b-lg z-[4]">
                   <h5 className="text-white font-semibold text-lg tracking-wide">
                     TPlayed
                   </h5>
