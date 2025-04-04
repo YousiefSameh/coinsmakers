4dda6f770a816666ef2cf69b13c65a43b3e12c02
Yousief Sameh <yousief.sameh@outlook.com>
2025-04-04 08:03:51 +0200

    Fix Model Open In Mobile And Change Responsive In Offerwall And Survey Partners

--- src/components/Home/FeaturedOffers.tsx
+++ src/components/Home/FeaturedOffers.tsx
@@ -15,14 +15,14 @@ const FeaturedOffers = () => {
 							key={index}
 							className="card bg-panel-bg p-3 flex justify-center flex-col hover:scale-105 transform transition duration-300"
 						>
-							<div className="relative group">
-								<div
-									className="overlay absolute top-0 left-0 h-full w-full backdrop-blur-[5px] hidden items-center justify-center text-none text-5xl z-[3] rounded-lg cursor-pointer group-hover:flex"
-									onClick={() => {
-										const modal = document.getElementById("my_modal_3");
-										if (modal) (modal as HTMLDialogElement).showModal();
-									}}
-								>
+							<div
+								className="relative group"
+								onClick={() => {
+									const modal = document.getElementById("my_modal_3");
+									if (modal) (modal as HTMLDialogElement).showModal();
+								}}
+							>
+								<div className="overlay absolute top-0 left-0 h-full w-full backdrop-blur-[5px] hidden items-center justify-center text-none text-5xl z-[3] rounded-lg cursor-pointer group-hover:flex">
 									<FaRegCirclePlay className="text-secondary-color" />
 								</div>
 								<div className="offers-devices absolute flex items-center bg-[#383d52d6] text-[1rem] text-[#ddd] justify-center gap-1 w-auto p-[5px] rounded-[5px] ml-[12px] z-[2] left-[-4px] top-[5px]">
@@ -83,77 +83,86 @@ const FeaturedOffers = () => {
 						</div>
 					</div>
 					<div className="content relative overflow-y-auto px-6 pt-4 bg-[#111923] max-h-[350px]">
-            <div className="about">
-              <span className="text-lg">About</span>
-              <hr className="border border-neutral-700" />
-              <div className="content bg-[#16202B] p-4 rounded-lg my-3">
-                <p className="text-base-content/60 text-sm">
-                  Alibaba. com is one of the world’s leading B2B ecommerce
-                  marketplaces. Our app allows you to source products from global
-                  suppliers, all from the convenience of your mobile device.
-                </p>
-              </div>
-            </div>
-            <div className="goals">
-              <span className="text-lg">Goals</span>
-              <hr className="border border-neutral-700" />
-              <div className="content flex flex-col gap-3 mt-4">
-                <div className="goal bg-[#16202B] p-4 rounded-lg flex items-center justify-between">
-                  <div className="goal-started flex items-center gap-2">
-                    <div
-                      aria-label="success"
-                      className="status status-success"
-                    ></div>
-                    <p className="text-base-content/60 text-sm">Open and use it.</p>
-                  </div>
-                  <div className="coins flex items-center gap-2">
-                    <span className="text-yellow-400 text-sm">
-                      <PiCoinVerticalFill />
-                    </span>
-                    <span className="text-sm">$200.00</span>
-                  </div>
-                </div>
-                <div className="goal bg-[#16202B] p-4 rounded-lg flex items-center justify-between">
-                  <div className="goal-started flex items-center gap-2">
-                    <div
-                      aria-label="success"
-                      className="status status-success"
-                    ></div>
-                    <p className="text-base-content/60 text-sm">Open and use it.</p>
-                  </div>
-                  <div className="coins flex items-center gap-2">
-                    <span className="text-yellow-400 text-sm">
-                      <PiCoinVerticalFill />
-                    </span>
-                    <span className="text-sm">$200.00</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="details mt-5">
-              <hr className="border border-neutral-700 outline-none" />
-              <div className="content flex items-center p-2 justify-between">
-                <div className="categories">
-                  <span className="block mb-2">Categories</span>
-                  <div className="flex items-center gap-2">
-                    <div className="badge badge-info text-white">Offers</div>
-                    <div className="badge badge-info text-white">Games</div>
-                  </div>
-                </div>
-                <div className="provider">
-                  <span className="block mb-2">Provider</span>
-                  <p className="text-base-content/60">Farly</p>
-                </div>
-                <div className="Mystatus flex flex-col">
-                  <span className="block mb-2">Status</span>
-                  <div className="badge badge-error text-white">Not Started</div>
-                </div>
-              </div>
-            </div>
+						<div className="about">
+							<span className="text-lg">About</span>
+							<hr className="border border-neutral-700" />
+							<div className="content bg-[#16202B] p-4 rounded-lg my-3">
+								<p className="text-base-content/60 text-sm">
+									Alibaba. com is one of the world’s leading B2B ecommerce
+									marketplaces. Our app allows you to source products from
+									global suppliers, all from the convenience of your mobile
+									device.
+								</p>
+							</div>
+						</div>
+						<div className="goals">
+							<span className="text-lg">Goals</span>
+							<hr className="border border-neutral-700" />
+							<div className="content flex flex-col gap-3 mt-4">
+								<div className="goal bg-[#16202B] p-4 rounded-lg flex items-center justify-between">
+									<div className="goal-started flex items-center gap-2">
+										<div
+											aria-label="success"
+											className="status status-success"
+										></div>
+										<p className="text-base-content/60 text-sm">
+											Open and use it.
+										</p>
+									</div>
+									<div className="coins flex items-center gap-2">
+										<span className="text-yellow-400 text-sm">
+											<PiCoinVerticalFill />
+										</span>
+										<span className="text-sm">$200.00</span>
+									</div>
+								</div>
+								<div className="goal bg-[#16202B] p-4 rounded-lg flex items-center justify-between">
+									<div className="goal-started flex items-center gap-2">
+										<div
+											aria-label="success"
+											className="status status-success"
+										></div>
+										<p className="text-base-content/60 text-sm">
+											Open and use it.
+										</p>
+									</div>
+									<div className="coins flex items-center gap-2">
+										<span className="text-yellow-400 text-sm">
+											<PiCoinVerticalFill />
+										</span>
+										<span className="text-sm">$200.00</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="details mt-5">
+							<hr className="border border-neutral-700 outline-none" />
+							<div className="content flex items-center p-2 justify-between">
+								<div className="categories">
+									<span className="block mb-2">Categories</span>
+									<div className="flex items-center gap-2">
+										<div className="badge badge-info text-white">Offers</div>
+										<div className="badge badge-info text-white">Games</div>
+									</div>
+								</div>
+								<div className="provider">
+									<span className="block mb-2">Provider</span>
+									<p className="text-base-content/60">Farly</p>
+								</div>
+								<div className="Mystatus flex flex-col">
+									<span className="block mb-2">Status</span>
+									<div className="badge badge-error text-white">
+										Not Started
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-          <div className="button px-6 py-4 bg-[#111923]">
-            <a href="#" className="btn btn-soft btn-lg btn-accent w-[100px]">Start</a>
-          </div>
+					<div className="button px-6 py-4 bg-[#111923]">
+						<a href="#" className="btn btn-soft btn-lg btn-accent w-[100px]">
+							Start
+						</a>
+					</div>
 				</div>
 			</dialog>
 		</section>
--- src/components/Home/SurveyPartners.tsx
+++ src/components/Home/SurveyPartners.tsx
@@ -15,14 +15,14 @@ const SurveyPartners = () => {
           return (
             <div
               key={index}
-              className="card p-3 flex justify-center flex-col hover:scale-105 transform transition duration-300"
+              className="card p-0 md:p-3 flex justify-center flex-col hover:scale-105 transform transition duration-300"
             >
-              <div className="relative w-full h-full bg-gradient-to-b from-[#1D232A] via-[#1D232A] to-[#1D232A] min-h-[13rem] min-w-[10rem] flex flex-col items-center justify-center rounded-lg shadow-lg hover:shadow-2xl hover:bg-gradient-to-b hover:from-[#3b7d67] transition-colors border border-[#3b7d67] group">
+              <div className="relative w-full h-full min-h-[13rem] min-w-[8rem] md:min-w-[10rem] bg-gradient-to-b from-[#1D232A] via-[#1D232A] to-[#1D232A] flex flex-col items-center justify-center rounded-lg shadow-lg hover:shadow-2xl hover:bg-gradient-to-b hover:from-[#3b7d67] transition-colors border border-[#3b7d67] group">
                 <div className="overlay absolute top-0 left-0 h-full w-full backdrop-blur-[5px] hidden items-center justify-center text-none text-5xl z-[3] rounded-lg cursor-pointer group-hover:flex" ><FaRegCirclePlay className="text-secondary-color" /></div>
                 <span className="badge badge-soft badge-success text-white absolute top-2 right-2">+20%</span>
                 <img
                   src="https://coinsmakers.com/asset/uploads/offerwalls/65b3ca0b736f21706281483.png"
-                  className="w-[100px] mb-6 hover:scale-110 transition-transform duration-300 drop-shadow-lg"
+                  className="w-[65px] md:w-[100px] mb-6 hover:scale-110 transition-transform duration-300 drop-shadow-lg"
                   alt="Card Image"
                 />
                 <div className="text absolute bottom-0 flex flex-col justify-center items-center gap-1 w-full py-2 rounded-b-lg z-[4]">
